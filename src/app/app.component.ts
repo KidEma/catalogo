733e03dd36c134ef2aeb859d7cb061891d92bc3f
ema.js <emilse.kida@gmail.com>
2018-08-29 20:43:08 -0300

    added favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'catalogo';
+  title = 'Catálogo';
   cons
   navBarDisplay = false;
 }
